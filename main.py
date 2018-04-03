f2da895e51c6bd40f33d5cd5cd88f62f4a81e7b7
Tyler Gabriel <tygabriel7@gmail.com>
2018-04-03 16:36:57 -0400

    1.0
    Added a main Method

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+from twython import Twython
+import time
+
+apiKey = "bWj659T4CGMKeP5N2q9sBLJW4"
+apiSecret = "CTgjgHsJl94cqdbAmQbpVjlAhiCWiHidUWSUbc0wyEU2vXeOo1"
+accessToken = "832293691888918533-BRQOh6vTVKfj7J1OBG0nGv2A0kXplcj"
+accessTokenSecret = "Kwt9SKhgZjqgITqtFRUrE6XU8YuCnFSxT8pRKNjRuDOll"
+days=1050;
+while True:
+    print '______________________________'
+
+    tweetStr='@KelenKeller38 @thestorysofarca @purenoiserecs It has been '+str(days)+' days since The Story So Far last put out a record. Where is it at?'
+    #api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
+    #api.update_status(status=tweetStr)
+    print ('Tweeted' + tweetStr)
+    print'______________________________________________'
+    days+=1
+    time.sleep(10)
